9bcf576a840def2296cd55b76bb2f5bcb81f0fdd
jeins <mail@mjuanakbar.info>
2016-01-26 21:30:15 +0100

    refactor routes

--- PKBackend/Api/Routes/RouteCollections/GeoServer/LayerRoute.php
+++ PKBackend/Api/Routes/RouteCollections/GeoServer/LayerRoute.php
@@ -4,12 +4,12 @@ use Phalcon\Mvc\Micro\Collection;
 
 return call_user_func(function(){
     $collection = new Collection();
-    $collection->setPrefix('/v1/gs/layer');
+    $collection->setPrefix('/v1/gs');
     $collection->setHandler('\PetaKami\Controllers\GeoServer\LayerController', true);
 
-    $collection->get('/', 'get');
-    $collection->post('/', 'postAction');
-    $collection->put('/{id}', 'putAction');
+    $collection->get('/layer', 'get');
+    $collection->post('/layer', 'postAction');
+    $collection->put('/layer/{id}', 'putAction');
 
     return $collection;
 });
--- PKBackend/Api/Routes/RouteCollections/GeoServer/WorkspaceRoute.php
+++ PKBackend/Api/Routes/RouteCollections/GeoServer/WorkspaceRoute.php
@@ -4,11 +4,11 @@ use Phalcon\Mvc\Micro\Collection;
 
 return call_user_func(function(){
     $collection = new Collection();
-    $collection->setPrefix('/v1/gs/workspace');
+    $collection->setPrefix('/v1/gs');
     $collection->setHandler('\PetaKami\Controllers\GeoServer\WorkspaceController', true);
 
-    $collection->get('/', 'getWorkspaces');
-    $collection->get('/{workspace}/draw', 'getWorkspaceWithDrawTyp');
+    $collection->get('/workspaces', 'getWorkspaces');
+    $collection->get('/workspace/{workspace}/draw', 'getWorkspaceWithDrawTyp');
 
     return $collection;
 });
