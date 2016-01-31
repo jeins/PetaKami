082cb88a5ee845e849f25dcc314e6ee5fda524b2
jeins <mail@mjuanakbar.info>
2016-01-31 19:37:35 +0100

    add alert if saved && allow save if name & geom already added

--- PKFrontend/app/scripts/sidenav/controllers/SideDrawCtrl.js
+++ PKFrontend/app/scripts/sidenav/controllers/SideDrawCtrl.js
@@ -4,67 +4,96 @@ angular.module('pkfrontendApp')
     .controller('SideDrawCtrl', [
         '$scope', 'svcWorkspace', 'svcSharedProperties', 'svcLayer',
         function ($scope, svcWorkspace, svcSharedProperties, svcLayer) {
-        var vm = this;
+            var vm = this;
 
-        var point = [], line=[], poly=[];
-        vm.setDrawTypes = [];
-        vm.setWorkspaces = [];
-        vm.selectedDrawType = '';
-        vm.layerGroupName = '';
-        vm.changeWorkspace = changeWorkspace;
-        vm.saveLayer = saveLayer;
+            var point = [], line=[], poly=[];
+            vm.setDrawTypes = [];
+            vm.setWorkspaces = [];
+            vm.selectedDrawType = '';
+            vm.layerGroupName = '';
+            vm.alerts = [];
+            vm.addAlert = addAlert;
+            vm.closeAlert = closeAlert;
+            vm.changeWorkspace = changeWorkspace;
+            vm.saveLayer = saveLayer;
+            vm.isDisabled = isDisabled;
 
-        init();
+            init();
 
-        function init(){
-            svcWorkspace.getWorkspaces(function(result){
-                vm.setWorkspaces = result.records;
-            });
+            function init(){
+                svcWorkspace.getWorkspaces(function(result){
+                    vm.setWorkspaces = result.records;
+                });
 
 
-            $scope.$on('pk.draw.feature', function(event, data){console.log("OK");
-                var feature = data;
-                switch(feature.getGeometry().getType()){
-                    case 'Point':
-                        if(point.id != feature.getProperties().id)
-                            point[feature.getProperties().id] = feature.getGeometry().getCoordinates();
-                        else point.id = data;
-                        break;
-                    case 'LineString':
-                        if(line.id != feature.getProperties().id)
-                            line[feature.getProperties().id] = feature.getGeometry().getCoordinates();
-                        else line.id = data;
-                        break;
-                    case 'Polygon':
-                        if(poly.id != feature.getProperties().id)
-                            poly[feature.getProperties().id] = feature.getGeometry().getCoordinates();
-                        else poly.id = data;
-                        break;
+                $scope.$on('pk.draw.feature', function(event, data){console.log("OK");
+                    var feature = data;
+                    switch(feature.getGeometry().getType()){
+                        case 'Point':
+                            if(point.id != feature.getProperties().id){
+                                point[feature.getProperties().id] = feature.getGeometry().getCoordinates();
+                            }
+                            else {
+                                point.id = data;
+                            }
+                            break;
+                        case 'LineString':
+                            if(line.id != feature.getProperties().id){
+                                line[feature.getProperties().id] = feature.getGeometry().getCoordinates();
+                            }
+                            else {
+                                line.id = data;
+                            }
+                            break;
+                        case 'Polygon':
+                            if(poly.id != feature.getProperties().id){
+                                poly[feature.getProperties().id] = feature.getGeometry().getCoordinates();
+                            }
+                            else {
+                                poly.id = data;
+                            }
+                            break;
+                    }
+                });
+            }
+
+            function isDisabled(text){
+                var tmpVal = svcSharedProperties.getLayerValues();
+                if(text == undefined || tmpVal == undefined){
+                    return true;
                 }
-            });
-        }
+                return false;
+            }
 
-        function changeWorkspace(workspace){
-            svcWorkspace.getWorkspaceWithDrawTyp(workspace, function(result){
-                vm.setDrawTypes = result.records;
-            });
-        }
+            function addAlert(layerGroupName){
+                vm.alerts.push({type: 'success', msg: 'Layer '+layerGroupName+' telah dibuat!'});
+            }
 
-        function saveLayer(workspace, layerGroupName){
-            var tmpVal = svcSharedProperties.getLayerValues();
+            function closeAlert(index){
+                vm.alerts.splice(index, 1);
+            }
 
-            var obj = {
-                "name": layerGroupName,
-                "workspace": workspace,
-                "type": {
-                    'point': tmpVal.point,
-                    'line': tmpVal.line,
-                    'poly': tmpVal.poly
-                }
-            };
+            function changeWorkspace(workspace){
+                svcWorkspace.getWorkspaceWithDrawTyp(workspace, function(result){
+                    vm.setDrawTypes = result.records;
+                });
+            }
 
-            svcLayer.addLayer(obj, function(response){
-                //TODO: what should i do after add layer?
-            });
-        }
+            function saveLayer(workspace, layerGroupName){
+                var tmpVal = svcSharedProperties.getLayerValues();
+
+                var obj = {
+                    "name": layerGroupName,
+                    "workspace": workspace,
+                    "type": {
+                        'point': tmpVal.point,
+                        'line': tmpVal.line,
+                        'poly': tmpVal.poly
+                    }
+                };
+
+                svcLayer.addLayer(obj, function(response){
+                    //TODO: what should i do after add layer?
+                });
+            }
     }]);
